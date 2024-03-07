26027a01b850d7c2ba319dd25b6bde9775cbbe3f
Hev X <Hev832@users.noreply.huggingface.co>
2024-03-07 08:28:46 +0000

    Update run.py

--- run.py
+++ run.py
@@ -36,7 +36,7 @@ app = gr.Interface(
         gr.Textbox(label="file name", placeholder="Defaults to video title"),
         gr.Dropdown(value="wav", label="format")
     ],
-    outputs=gr.File(label="Download the file!"),
+    outputs=gr.Audio(label="Download the file!"),
     description="<div style='font-size:30px; text-align:center;'>YouTube wav downloader</div>"
 )
 
