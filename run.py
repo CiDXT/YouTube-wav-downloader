d882bc06b2503d26972f50702f16be1d033a79e5
Blane <161300198+Blane187@users.noreply.github.com>
2024-03-23 11:47:53 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,43 +1,39 @@
 import gradio as gr
-import os
-import subprocess
-from IPython.display import Audio
-
-def get_video_title(url):
-    # 使用 yt-dlp 获取视频标题
-    result = subprocess.run(["yt-dlp", "--get-title", url], capture_output=True, text=True)
-    if result.returncode == 0:
-        return result.stdout.strip()
-    else:
-        return "Unknown Video"
-
-def fetch(url, custom_name, ext):
-    title = get_video_title(url)
-    # 截断标题为一个合理的长度
-    max_length = 50  # 调整为适当的值
-    truncated_title = title[:max_length].strip()
-    
-    filename = f"{custom_name}.{ext}" if custom_name else f"{truncated_title}.{ext}"
-    opts = {
-        "wav": ["-f", "ba", "-x", "--audio-format", "wav"],
-        "mp4": ["-f", "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best"],
-    }[ext]
-    command = ["yt-dlp"] + opts + [url, "-o", filename]
-    subprocess.run(command)
-
-    return filename
+import yt_dlp
+import ffmpeg
 
 
-app = gr.Interface(
-    theme='Hev832/soft',
-    fn=fetch,
-    inputs=[
-        gr.Textbox(label="YouTube video address", placeholder="Paste video link here..."),
-        gr.Textbox(label="file name", placeholder="Defaults to video title"),
-        gr.Dropdown(value="wav", label="format")
-    ],
-    outputs=gr.Audio(label="Download the file!"),
-    description="<div style='font-size:30px; text-align:center;'>YouTube wav downloader...</div>"
-)
+def download_audio(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'wav',
+            'preferredquality': '192',
+        }],
+        'outtmpl': '%(title)s.%(ext)s',
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    return "audio downloaded as '%(title)s.wav'"
 
-app.launch(debug=True, share=True)
+
+with gr.Blocks(theme="Hev832/soft", title="yt-dlp") as demo:    
+    used_letters_var = gr.State([])
+    with gr.Column():
+        gr.Markdown("# YT_DLP GRADIO DEMO")
+        gr.Markdown("Please press like button to support me :]")
+    with gr.Row() as row:
+        with gr.Column():
+            url = gr.Textbox(label="URL INPUT")
+            
+        with gr.Column():
+            btn = gr.Button("download!")
+            outputs = gr.Audio(label="outputs")
+
+    btn.click(
+        download_audio, 
+        [url],
+        [outputs]
+        )
+demo.launch(debug=True, share=True)
