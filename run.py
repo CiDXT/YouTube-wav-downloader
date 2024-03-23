53ba43f6af0d9f2a1c7dea3c84cfd6d6ef5b2b71
Blane <161300198+Blane187@users.noreply.github.com>
2024-03-23 12:05:10 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,37 +1,50 @@
 import gradio as gr
-import yt_dlp
+import os
+import subprocess
+
+def get_video_title(url):
+     
+    result = subprocess.run(["yt-dlp", "--get-title", url], capture_output=True, text=True)
+    if result.returncode == 0:
+        return result.stdout.strip()
+    else:
+        return "Unknown Video"
+
+def fetch(url, custom_name, ext):
+    title = get_video_title(url)
+    #  
+    max_length = 50  # 
+    truncated_title = title[:max_length].strip()
+    
+    filename = f"{custom_name}.{ext}" if custom_name else f"{truncated_title}.{ext}"
+    opts = {
+        "mp3": ["-f", "ba", "-x", "--audio-format", "mp3"],
+        "wav": ["-f", "ba", "-x", "--audio-format", "wav"],
+
+    }[ext]
+    command = ["yt-dlp"] + opts + [url, "-o", filename]
+    subprocess.run(command)
+
+    return filename
 
 
-def download_audio(url):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'wav',
-            'preferredquality': '192',
-        }],
-        'outtmpl': '%(title)s.%(ext)s',
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-    return "audio downloaded as '%(title)s.wav'"
 
-
-with gr.Blocks(theme="Hev832/soft", title="yt-dlp") as demo:    
+with gr.Blocks() as demo:    
     used_letters_var = gr.State([])
     with gr.Column():
         gr.Markdown("# YT_DLP GRADIO DEMO")
-        gr.Markdown("Please press stars ⭐ button on github to support me :]")
+        gr.Markdown("Please press like button to support me :]")
     with gr.Row() as row:
         with gr.Column():
             url = gr.Textbox(label="URL INPUT")
-            
+            custom_name = gr.Textbox(label="Custom Name (defalut if you want!)")
+            outputs = gr.Audio(label="outputs")
         with gr.Column():
             btn = gr.Button("download!")
-            outputs = gr.Audio(label="outputs")
+            
 
     btn.click(
-        download_audio, 
+        fetch, 
         [url],
         [outputs]
         )
